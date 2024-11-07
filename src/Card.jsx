089395d607b7a49c73032770adf30d8dd77f0238
LuisRiofrio <lriofrio915@gmail.com>
2024-11-07 18:33:59 -0500

    se agregan variables de estado al formulario

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,11 @@
 //Este componente deberia recibir por props y mostrar en pantalla la informacion
 //que envia el usuario
 
-function Card() {
+function Card({input1, input2}) {
   return (
     <div>
-      <h2>Esto es un componente</h2>
+      <h2>Información Ingresada Exitosamente</h2>
+      <p>Bienvenido {input1} {input2} </p>
     </div>
   );
 }
